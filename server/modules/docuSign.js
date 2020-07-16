272e321ab26ca878d34253fcba6bc00ad96453fe
jsandleraol <46496940+jsandleraol@users.noreply.github.com>
2020-07-16 13:50:26 -0700

    first commit

--- server/modules/docuSign.js
+++ server/modules/docuSign.js
@@ -1,67 +1,11 @@
-async function sendEnvelopeController (req, res) {
-    const qp = req.query;
-    console.log("qp:", qp);
-  
-    const accessToken = qp.ACCESS_TOKEN;
-    const accountId = qp.ACCOUNT_ID; 
-    const signerName = qp.USER_FULLNAME;
-    const signerEmail = qp.USER_EMAIL;
-    
-    const fileName = 'demo_documents/World_Wide_Corp_lorem.pdf';
+const privateKeyFilename = "private.key";
+const oAuthBasePath = "account-d.docusign.com";
+const clientId = "961c2672-828d-489f-980f-008ff6b37c60";
+const userId = "9d4daa54-97ef-4750-9aae-4ba3422ff00a";
+const expiresIn = 320000;
 
-    const apiClient = new docusign.ApiClient();
-    apiClient.setBasePath(basePath);
-    apiClient.addDefaultHeader('Authorization', 'Bearer ' + accessToken);
-    docusign.Configuration.default.setDefaultApiClient(apiClient);
-  
-    const envDef = new docusign.EnvelopeDefinition();
-    envDef.emailSubject = 'Example form DocuSign Subject';
-    envDef.emailBlurb = 'Example form DocuSign Blurb'
-  
-    const pdfBytes = fs.readFileSync(path.resolve(__dirname, fileName))
-        , pdfBase64 = pdfBytes.toString('base64');
-    
-    const doc = docusign.Document.constructFromObject({documentBase64: pdfBase64,
-          fileExtension: 'pdf',
-          name: 'Sample document', documentId: '1'});
-  
-    envDef.documents = [doc];
-  
-    const signer = docusign.Signer.constructFromObject({name: signerName,
-          email: signerEmail, routingOrder: '1', recipientId: '1'});
-  
-    const signHere = docusign.SignHere.constructFromObject({documentId: '1',
-          pageNumber: '1', recipientId: '1', tabLabel: 'SignHereTab',
-          xPosition: '195', yPosition: '147'});
+function configureJWTAuthorizationFlow(privateKeyFilename, oAuthBasePath, clientId, userId, expiresIn, callback){
 
-    signer.tabs = docusign.Tabs.constructFromObject({signHereTabs: [signHere]});
-  
-    envDef.recipients = docusign.Recipients.constructFromObject({signers: [signer]});
-    envDef.status = 'sent';
-   
-    let envelopesApi = new docusign.EnvelopesApi()
-      , createEnvelopePromise = promisify(envelopesApi.createEnvelope).bind(envelopesApi)
-      , results
-      ;
-  
-    try {
-      results = await createEnvelopePromise(accountId, {'envelopeDefinition': envDef})
-    } catch  (e) {
-      let body = e.response && e.response.body;
-      if (body) {
-        res.send (`<html lang="en"><body>
-                    <h3>API problem</h3><p>Status code ${e.response.status}</p>
-                    <p>Error message:</p><p><pre><code>${JSON.stringify(body, null, 4)}</code></pre></p>`);
-      } else {
-        throw e;
-      }
-    }
-    if (results) {
-      res.send (`<html lang="en"><body>
-                  <h3>Envelope Created!</h3>
-                  <p>Signer: ${signerName} &lt;${signerEmail}&gt;</p>
-                  <p>Results</p><p><pre><code>${JSON.stringify(results, null, 4)}</code></pre></p>`);
-    }
-  };
+};
 
-module.exports = sendEnvelopeController;
+module.exports = configureJWTAuthorizationFlow;

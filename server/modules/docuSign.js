d5d2da0a21f6436b2b344d621247efc7a5398234
jsandleraol <46496940+jsandleraol@users.noreply.github.com>
2020-07-16 14:20:18 -0700

    docuSign module made

--- server/modules/docuSign.js
+++ server/modules/docuSign.js
@@ -1,11 +1,13 @@
+const docuSign = require('docusign-esign');
+
 const privateKeyFilename = "private.key";
 const oAuthBasePath = "account-d.docusign.com";
 const clientId = "961c2672-828d-489f-980f-008ff6b37c60";
 const userId = "9d4daa54-97ef-4750-9aae-4ba3422ff00a";
 const expiresIn = 320000;
 
-function configureJWTAuthorizationFlow(privateKeyFilename, oAuthBasePath, clientId, userId, expiresIn, callback){
+function makeCall() {
+  docuSign.configureJWTAuthorizationFlow(privateKeyFilename, oAuthBasePath, clientId, userId, expiresIn, callback)
+}
 
-};
-
-module.exports = configureJWTAuthorizationFlow;
+module.exports = makeCall;

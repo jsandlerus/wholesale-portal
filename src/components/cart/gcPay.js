30dbc3df80ae4d725d640c2175fca66e6325ad98
ReverendEd <tysehr37@gmail.com>
2020-08-15 12:50:04 -0700

    fixed custom order bugs and added error handling, updated messages on client

--- src/components/cart/gcPay.js
+++ src/components/cart/gcPay.js
@@ -291,7 +291,7 @@ class GCPay extends React.Component {
     } else {
       return (
         <div>
-          <h1>Payment Done!</h1>
+          <h1>PAYMENT DONE!</h1>
         </div>
       )
     }

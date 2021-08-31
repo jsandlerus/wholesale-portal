d237fcc0037ae94074266fa77edb472b046fa98d
jsandleraol <46496940+jsandleraol@users.noreply.github.com>
2021-08-30 21:45:07 -0700

    Updates

---
+++ precache-manifest.0f42aa14e50090fec1e3c0b02638bf78.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0dac49f80174365f8f6f453e149c22",
+    "url": "/wholesale-portal/index.html"
+  },
+  {
+    "revision": "2144a8c696a47153d48b",
+    "url": "/wholesale-portal/static/css/main.8b8d5f2c.chunk.css"
+  },
+  {
+    "revision": "d48b365a51b3d0233267",
+    "url": "/wholesale-portal/static/js/2.526f7362.chunk.js"
+  },
+  {
+    "revision": "2825d23a41400caf4cae056cacb996a4",
+    "url": "/wholesale-portal/static/js/2.526f7362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2144a8c696a47153d48b",
+    "url": "/wholesale-portal/static/js/main.52196214.chunk.js"
+  },
+  {
+    "revision": "83a07a733a94fdc554ad",
+    "url": "/wholesale-portal/static/js/runtime-main.3f62a953.js"
+  },
+  {
+    "revision": "ef46ab607c00342baa76086d0c804c9f",
+    "url": "/wholesale-portal/static/media/cbddy_logo_small.ef46ab60.png"
+  },
+  {
+    "revision": "2a237eedd0f1f85d52d69505a34e33c8",
+    "url": "/wholesale-portal/static/media/loading.2a237eed.svg"
+  },
+  {
+    "revision": "4e5d136588498896ca5a4cc97e0a292e",
+    "url": "/wholesale-portal/static/media/loadingBig.4e5d1365.svg"
+  }
+]);
